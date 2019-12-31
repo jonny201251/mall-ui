bb762d461255c18aa1c5b42e6a886e69b908f7f1
jonny201251 <jonny201251@163.com>
2019-12-31 18:00:31 +0800

    更新

--- src/pages/Item/ItemAdd/ItemAdd.js
+++ src/pages/Item/ItemAdd/ItemAdd.js
@@ -2,23 +2,36 @@ import React, {PureComponent} from 'react'
 import {Button, message, Steps, Breadcrumb, Card, Icon} from 'antd'
 import Form, {FormCore, FormItem} from 'noform'
 import {Input, Radio, TreeSelect, Select, InputNumber, Upload} from 'nowrapper/lib/antd'
+import {InlineRepeater, Selectify} from 'nowrapper/lib/antd/repeater'
 import request from '../../../utils/request'
 import uploadStyle from './upload.less'
-import styles from "../../ImageRichText/upload.less";
+//editor
+import 'braft-editor/dist/index.css'
+import BraftEditor from 'braft-editor'
+import {ContentUtils} from 'braft-utils'
+
+const SelectInlineRepeater = Selectify(InlineRepeater)
 
 const validate = {
-    name: {type: "number", required: true, message: '类目名称不能为空'}
+    categoryId: {type: "number", required: true, message: '商品类目不能为空'},
+    title: {type: "string", required: true, message: '商品标题不能为空'},
+    brandId: {type: "number", required: true, message: '品牌不能为空'},
+    tmpPrice: {type: "number", required: true, message: '商品价格不能为空'},
+    tmpStock: {type: "number", required: true, message: '商品库存不能为空'},
 }
 
 const categoryPath = '/mall/category'
 const spuPath = '/mall/spu'
 const brandPath = '/mall/brand'
+const hostPath = 'http://localhost:8082/mall'
 export default class ItemAdd extends PureComponent {
     state = {
         treeSelectData: [],
         //商品图片
         fileList: [],
         defaultFileList: [],
+        //editor
+        editorState: BraftEditor.createEditorState(''),
     }
 
     constructor(props) {
@@ -40,6 +53,39 @@ export default class ItemAdd extends PureComponent {
         })
     }
 
+    //editor
+    handleChange = (editorState) => {
+        this.setState({
+            editorState: editorState,
+            outputHTML: editorState.toHTML()
+        })
+    }
+    uploadHandler = (param) => {
+        if (!param.file) {
+            return false
+        }
+        if (param.file.type.indexOf('image') === -1) {
+            message.warning('上传的不是图片')
+            return
+        }
+        //将图片上传到后台
+        const formData = new FormData();
+        formData.append('imageFile', param.file)
+        //异步请求
+        request.post(spuPath + '/uploadImage', {data: formData}).then(res => {
+            if (res && res.code === 1) {
+                this.setState({
+                    editorState: ContentUtils.insertMedias(this.state.editorState, [{
+                        type: 'IMAGE',
+                        url: hostPath + res.data
+                    }])
+                })
+            } else {
+                message.error("上传图片失败！")
+            }
+        })
+    }
+
     componentWillMount() {
         //取出 商品类目
         request.get(categoryPath + '/treeSelect').then(res => {
@@ -52,7 +98,6 @@ export default class ItemAdd extends PureComponent {
     showCategoryNames = () => {
         return this.state.categoryNames.map((name) => <Breadcrumb.Item>{name}</Breadcrumb.Item>)
     }
-
     onSelect = (value, node, extra) => {
         let categoryId = value
         this.setState({categoryId})
@@ -82,29 +127,60 @@ export default class ItemAdd extends PureComponent {
     }
 
     onClick = () => {
-        //准备附件数据
-        const formData = new FormData();
-        this.state.fileList.forEach((file) => {
-            formData.append('files', file)
-        })
-        //将表单数据放入formData
-        formData.append("form", JSON.stringify(this.core.getValues()))
-        //异步请求
-        request.post(spuPath + '/add', {data: formData}).then(res => {
-            if (res && res.code === 1) {
-                message.success("操作成功")
-            } else {
-                message.error("操作失败")
+        console.log(this.core.getValues())
+        this.core.validate((err) => {
+            if (!err) {
+                //校验商品图片
+                if (this.state.fileList.length === 0) {
+                    message.warning('请上传商品图片')
+                    return
+                }
+                //准备附件数据
+                const formData = new FormData();
+                this.state.fileList.forEach((file) => {
+                    formData.append('files', file)
+                })
+                //商品描述数据
+                formData.append('description', this.state.outputHTML)
+                //表单数据
+                formData.append("form", JSON.stringify(this.core.getValues()))
+                //异步请求
+                request.post(spuPath + '/add', {data: formData}).then(res => {
+                    if (res && res.code === 1) {
+                        message.success("操作成功")
+                    } else {
+                        message.error("操作失败")
+                    }
+                })
             }
         })
     }
 
     render() {
+        //editor
+        const extendControls = [
+            {
+                key: 'antd-uploader',
+                type: 'component',
+                component: (
+                    <Upload
+                        accept="image/*"
+                        showUploadList={false}
+                        customRequest={this.uploadHandler}
+                    >
+                        {/* 这里的按钮最好加上type="button"，以避免在表单容器中触发表单提交，用Antd的Button组件则无需如此 */}
+                        <button type="button" className="control-item button upload-button" data-title="插入图片">
+                            <Icon type="picture" theme="filled"/>
+                        </button>
+                    </Upload>
+                )
+            }
+        ]
         return (
             <div>
                 <Form core={this.core}>
                     <FormItem style={{display: 'none'}} name="id"><Input/></FormItem>
-                    <Card title='请选择商品类目'>
+                    <Card title='商品的类目'>
                         <FormItem name="categoryId">
                             <TreeSelect size={'large'} treeData={this.state.treeSelectData} treeDefaultExpandAll
                                         onSelect={(value, node, extra,) => this.onSelect(value, node, extra)}/>
@@ -115,13 +191,12 @@ export default class ItemAdd extends PureComponent {
                             </Breadcrumb>
                         </div>
                     </Card>
-                    <Card title='填写商品信息' style={{marginTop: 20}}>
+                    <Card title='商品的基本信息' style={{marginTop: 10}}>
                         <FormItem label="商品标题" name="title" required={true}><Input/></FormItem>
-                        <FormItem label="副标题" name="subTitle" required={true}><Input/></FormItem>
-                        <FormItem label="品牌" name="brandId"><Select options={this.state.brandSelectOptions}
-                                                                    required={true}/></FormItem>
-                        <FormItem label="商品价格" name="tmpPrice" required={true}><InputNumber/></FormItem>
-                        <FormItem label="商品库存" name="tmpStock" required={true}><InputNumber/></FormItem>
+                        <FormItem label="副标题" name="subTitle"><Input/></FormItem>
+                        <FormItem label="品牌" name="brandId" required={true}><Select
+                            options={this.state.brandSelectOptions}
+                            required={true}/></FormItem>
                         <FormItem label="商品图片" required={true}/>
                         <div style={{width: 400}}>
                             <Upload.Dragger listType='picture'
@@ -132,9 +207,40 @@ export default class ItemAdd extends PureComponent {
                                 </p>
                             </Upload.Dragger>
                         </div>
+                        <FormItem label="包装清单" name="packingList"><Input.TextArea/></FormItem>
+                        <FormItem label="售后服务" name="afterService"><Input.TextArea/></FormItem>
+                    </Card>
+                    <Card title='商品的描述' style={{marginTop: 10}}>
+                        <div className="editor-wrapper">
+                            <BraftEditor
+                                value={this.state.editorState}
+                                onChange={this.handleChange}
+                                extendControls={extendControls}
+                                excludeControls={['media', 'fullscreen']}
+                                contentStyle={{height: 400}}
+                            />
+                        </div>
+                    </Card>
+                    <Card title='商品的通用属性' style={{marginTop: 10}}>
+
+                    </Card>
+                    <Card title='商品的特有属性' style={{marginTop: 10}}>
+
+                    </Card>
+                    <Card title='商品的其他属性' style={{marginTop: 10}}>
+                        <FormItem name="spec_seller_define">
+                            <SelectInlineRepeater locale='zh' selectMode="multiple" multiple>
+                                <FormItem label='属性名称' name="name"><Input/></FormItem>
+                                <FormItem label='属性值' name="value"><Input/></FormItem>
+                            </SelectInlineRepeater>
+                        </FormItem>
+                    </Card>
+                    <Card title='商品的价格、库存' style={{marginTop: 10}}>
+                        <FormItem label="商品价格" name="tmpPrice" required={true}><InputNumber/></FormItem>
+                        <FormItem label="商品库存" name="tmpStock" required={true}><InputNumber/></FormItem>
                     </Card>
                     <div style={{marginTop: 20}}>
-                        <Button type="primary" onClick={this.onClick}>发布</Button>
+                        <Button size='large' type="primary" onClick={this.onClick} style={{width:200}}>发布</Button>
                     </div>
                 </Form>
             </div>
